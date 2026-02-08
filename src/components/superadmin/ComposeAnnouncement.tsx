0f9400bbb043ccdc393f1ecd6ae0b36496942127
Ideapad - Quad Core <09iqrasiddique>
2026-02-08 02:06:17 -0800

    solve some error

--- src/components/superadmin/ComposeAnnouncement.tsx
+++ src/components/superadmin/ComposeAnnouncement.tsx
@@ -10,12 +10,6 @@ const BackIcon = () => (
     </svg>
 );
 
-const SendIcon = () => (
-    <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.16669 9.83333L9.50002 13.1667L18.6667 4" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-);
-
 const SendPlaneIcon = () => (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
         <path d="M7.39999 6.32003L15.89 3.49003C19.7 2.22003 21.77 4.30003 20.51 8.11003L17.68 16.6C15.78 22.31 12.66 22.31 10.76 16.6L9.91999 14.08L7.39999 13.24C1.68999 11.34 1.68999 8.23003 7.39999 6.32003Z" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
